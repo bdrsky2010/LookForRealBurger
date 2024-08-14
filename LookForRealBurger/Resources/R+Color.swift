98430475b60eb7e0c2a577636d389984db4143bd
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-14 18:26:33 +0900

    💾 Add: 따로 에셋 추가한 컬러 리소스 타입 프로퍼티로 추가

---
+++ LookForRealBurger/Resources/R+Color.swift
@@ -0,0 +1,20 @@
+//
+//  R+Color.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/14/24.
+//
+
+import UIKit.UIColor
+
+extension R {
+    enum Color { }
+}
+
+extension R.Color {
+    static let white = UIColor.appWhite
+    static let background = UIColor.appBackground
+    static let red = UIColor.appRed
+    static let brown = UIColor.appBrown
+    static let orange = UIColor.appOrange
+}
