604dba80e0bcb1e6a6b2b9d2e70e09658b4529e9
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-14 18:27:29 +0900

    💾 Add: 앱 내에서 사용할 커스텀 폰트 리소스 열거형 내에 타입 프로퍼티로 구성

---
+++ LookForRealBurger/Resources/R+Font.swift
@@ -0,0 +1,38 @@
+//
+//  R+Font.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/14/24.
+//
+
+import UIKit.UIFont
+
+extension R {
+    enum Font { }
+}
+
+extension R.Font {
+    static let chab13 = UIFont.chap(size: 13)
+    static let chab14 = UIFont.chap(size: 14)
+    static let chab15 = UIFont.chap(size: 15)
+    static let chab16 = UIFont.chap(size: 16)
+    static let chab50 = UIFont.chap(size: 50)
+    
+    static let regular12 = UIFont.pretendard(size: 12, weight: .regular)
+    static let regular13 = UIFont.pretendard(size: 13, weight: .regular)
+    static let regular14 = UIFont.pretendard(size: 14, weight: .regular)
+    static let regular15 = UIFont.pretendard(size: 15, weight: .regular)
+    static let regular16 = UIFont.pretendard(size: 16, weight: .regular)
+    
+    static let bold12 = UIFont.pretendard(size: 12, weight: .bold)
+    static let bold13 = UIFont.pretendard(size: 13, weight: .bold)
+    static let bold14 = UIFont.pretendard(size: 14, weight: .bold)
+    static let bold15 = UIFont.pretendard(size: 15, weight: .bold)
+    static let bold16 = UIFont.pretendard(size: 16, weight: .bold)
+    
+    static let black12 = UIFont.pretendard(size: 12, weight: .black)
+    static let black13 = UIFont.pretendard(size: 13, weight: .black)
+    static let black14 = UIFont.pretendard(size: 14, weight: .black)
+    static let black15 = UIFont.pretendard(size: 15, weight: .black)
+    static let black16 = UIFont.pretendard(size: 16, weight: .black)
+}
