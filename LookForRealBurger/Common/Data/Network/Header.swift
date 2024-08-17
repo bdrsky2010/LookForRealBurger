0983cb03977babe423c5dc0c150ed9ab19fdc512
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-17 23:26:04 +0900

    💾 Add: API 네트워크 통신할 떄 사용할 Header 구성

---
+++ LookForRealBurger/Common/Data/Network/Header.swift
@@ -0,0 +1,17 @@
+//
+//  Header.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/17/24.
+//
+
+import Foundation
+
+enum Header: String {
+    case authorization = "Authorization"
+    case sesacKey      = "SesacKey"
+    case refresh       = "Refresh"
+    case contentType   = "Content-Type"
+    case json          = "application/json"
+    case multipart     = "multipart/form-data"
+}
