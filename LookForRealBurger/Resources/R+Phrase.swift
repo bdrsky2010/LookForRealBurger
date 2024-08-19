5ac5e28d9f9c931bee6a11a0d4d49852301677f1
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-19 23:35:20 +0900

    💾 Add: 앱 내에 들어가는 문구도 리소스에 구성

---
+++ LookForRealBurger/Resources/R+Phrase.swift
@@ -0,0 +1,17 @@
+//
+//  R+Phrase.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/19/24.
+//
+
+import Foundation
+
+extension R {
+    enum Phrase { }
+}
+
+extension R.Phrase {
+    static let errorOccurred = "errorOccurred".localized
+    static let networkUnstable = "networkUnstable".localized
+}
