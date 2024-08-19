8319321aeb1a67b0f44e72172e24e2abf3ab37b6
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-19 23:36:52 +0900

    💾 Add: 회원가입 시 Query로 사용되는 타입 구성

---
+++ LookForRealBurger/Scene/Join/Domain/Entities/EmailQuery.swift
@@ -0,0 +1,12 @@
+//
+//  EmailQuery.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/19/24.
+//
+
+import Foundation
+
+struct EmailQuery {
+    let email: String
+}
---
+++ LookForRealBurger/Scene/Join/Domain/Entities/JoinQuery.swift
@@ -0,0 +1,14 @@
+//
+//  JoinQuery.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/19/24.
+//
+
+import Foundation
+
+struct JoinQuery {
+    let email: String
+    let password: String
+    let nick: String
+}
