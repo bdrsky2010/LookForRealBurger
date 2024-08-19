466d23a0984bf0b82b050fbe24cae966eecc12d8
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-19 23:31:01 +0900

    💾 Add: UINavigationBar appearance 세팅하는 타입 메서드 구성

---
+++ LookForRealBurger/Application/AppAppearance.swift
@@ -0,0 +1,23 @@
+//
+//  AppAppearance.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/19/24.
+//
+
+import UIKit
+
+enum AppAppearance {
+    static func setupAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .clear
+        appearance.titleTextAttributes = [
+            NSAttributedString.Key.font: R.Font.chab20,
+            NSAttributedString.Key.foregroundColor: R.Color.brown
+        ]
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+}
