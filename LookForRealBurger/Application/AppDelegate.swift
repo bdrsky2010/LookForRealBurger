5f543132302a959e168089a72fab5d95b9c8a1a4
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-20 22:48:06 +0900

    💾 Add: 런치스크린 1초간 유지

--- LookForRealBurger/Application/AppDelegate.swift
+++ LookForRealBurger/Application/AppDelegate.swift
@@ -11,9 +11,8 @@ import IQKeyboardManagerSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        sleep(1)
         AppAppearance.setupAppearance()
         IQKeyboardManager.shared.enable = true
         return true
