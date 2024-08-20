ca91a0622c70bf9fa0a071b03c4e75aae2e00693
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-20 22:48:59 +0900

    🗑️ Remove: 파일 합침

--- LookForRealBurger/Common/Data/AccessTokenStorage/AccessTokenStorage.swift
+++ LookForRealBurger/Common/Data/AccessTokenStorage/AccessTokenStorage.swift
@@ -17,3 +17,55 @@ protocol AccessTokenStorage: AnyObject {
     func saveToken(for tokenType: TokenType, with token: String)
     func removeToken()
 }
+
+final class UserDefaultsAccessTokenStorage {
+    static let shared = UserDefaultsAccessTokenStorage()
+    
+    private let accessTokenKey = "LookForRealBurgerAccess"
+    private let refreshTokenKey = "LookForRealBurgerRefresh"
+    private let userDefaults: UserDefaults
+    
+    private init(
+        userDefaults: UserDefaults = UserDefaults.standard
+    ) {
+        self.userDefaults = userDefaults
+    }
+    
+    private func resetUserDefaults() {
+        for key in userDefaults.dictionaryRepresentation().keys {
+            userDefaults.removeObject(forKey: key.description)
+        }
+    }
+}
+
+extension UserDefaultsAccessTokenStorage: AccessTokenStorage {
+    func fetchToken(for tokenType: TokenType) -> String {
+        let token: String
+        
+        switch tokenType {
+        case .access:
+            token = userDefaults.string(forKey: accessTokenKey) ?? ""
+        case .refresh:
+            token = userDefaults.string(forKey: refreshTokenKey) ?? ""
+        }
+        
+        return token
+    }
+    
+    func saveToken(for tokenType: TokenType, with token: String) {
+        let tokenKey: String
+        
+        switch tokenType {
+        case .access:
+            tokenKey = accessTokenKey
+        case .refresh:
+            tokenKey = refreshTokenKey
+        }
+        
+        userDefaults.set(token, forKey: tokenKey)
+    }
+    
+    func removeToken() {
+        resetUserDefaults()
+    }
+}
