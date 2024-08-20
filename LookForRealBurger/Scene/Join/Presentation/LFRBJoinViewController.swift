f71e4f9489045e4a616d5d4084a4a9bac3d94fc6
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-20 23:07:52 +0900

    💾 Add: 회원가입 화면 구성
    #1 textField의 text들은 변화가 감지될 때마다 공백을 제거해줌
    
    #2 이메일로 입력된 text 형태가 이메일의 형태를 갖추고 있을 시 이메일 중복검사 버튼 활성화
    
    #3 이메일 중복검사 버튼 클릭 시, API 네트워크 통신을 통하여 중복에 대한 검사를 하여 에러 발생 시 토스트메시지를 통해 사용자에게 시각적으로 제공, 중복된 이메일이거나 사용가능한 이메일이면 사용자에게 토스트 메시지를 통해 알려줌
    
    #4 패스워드 textField secure한 text로 보이도록 수정
    
    #5 이메일, 비밀번호, 닉네임이 비어있지 않고 이메일이 중복되지 않은 이메일인 것이 검증이 되면 회원가입 버튼 활성화
    
    #6 아직 미완성이지만 회원가입 버튼 클릭 시, API 네트워크 통신을 통해 회원가입을 진행 및 위와 동일하게 에러 발생 시 토스트 메시지를 통해 알려줌

---
+++ LookForRealBurger/Scene/Join/Presentation/LFRBJoinViewController.swift
@@ -0,0 +1,247 @@
+//
+//  LFRBJoinViewController.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/18/24.
+//
+
+import UIKit
+import Lottie
+import RxCocoa
+import RxSwift
+import SnapKit
+import Toast
+
+final class LFRBJoinViewController: BaseViewController {
+    private let baseView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    private let bunUpImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "bunUp")?.withRenderingMode(.alwaysOriginal)
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let emailValidImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "checkmark")
+        imageView.preferredSymbolConfiguration = .init(font: UIFont.systemFont(ofSize: 14, weight: .bold))
+        imageView.tintColor = R.Color.green
+        imageView.isHidden = true
+        return imageView
+    }()
+    
+    private let emailSearchBar = BorderRoundedSearchBar(
+        borderWidth: 4, borderColor: R.Color.green, placeholder: "아이디(이메일)"
+    )
+    private let emailValidButton = CapsuleButton(
+        title: "이메일 중복 검사", backgroudColor: R.Color.red
+    )
+    private let passwordSearchBar = BorderRoundedSearchBar(
+        borderWidth: 4, borderColor: R.Color.yellow, placeholder: "비밀번호"
+    )
+    private let nickSearchBar = BorderRoundedSearchBar(
+        borderWidth: 4, borderColor: R.Color.brown, placeholder: "닉네임"
+    )
+    private let joinButton = CapsuleButton(
+        title: "회원가입", backgroudColor: R.Color.green
+    )
+    
+    private let bunDownImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "bunDown")?.withRenderingMode(.alwaysOriginal)
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let disposeBag = DisposeBag()
+    
+    private var viewModel: JoinViewModel!
+    
+    static func create(
+        with viewModel: JoinViewModel
+    ) -> LFRBJoinViewController {
+        let view = LFRBJoinViewController()
+        view.viewModel = viewModel
+        return view
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(baseView)
+        baseView.addSubview(bunUpImageView)
+        baseView.addSubview(emailSearchBar)
+        baseView.addSubview(emailValidImage)
+        baseView.addSubview(emailValidButton)
+        baseView.addSubview(passwordSearchBar)
+        baseView.addSubview(nickSearchBar)
+        baseView.addSubview(joinButton)
+        baseView.addSubview(bunDownImageView)
+    }
+    
+    override func configureLayout() {
+        baseView.snp.makeConstraints { make in
+            make.centerY.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        bunUpImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.height.equalTo(105)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+  
+        emailSearchBar.snp.makeConstraints { make in
+            make.top.equalTo(bunUpImageView.snp.bottom).offset(12)
+            make.height.equalTo(50)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        emailValidImage.snp.makeConstraints { make in
+            make.trailing.equalTo(emailSearchBar.snp.trailing).inset(10)
+            make.centerY.equalTo(emailSearchBar.snp.centerY)
+            make.size.equalTo(20)
+        }
+        
+        emailValidButton.snp.makeConstraints { make in
+            make.top.equalTo(emailSearchBar.snp.bottom).offset(12)
+            make.height.equalTo(50)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+
+        passwordSearchBar.snp.makeConstraints { make in
+            make.top.equalTo(emailValidButton.snp.bottom).offset(12)
+            make.height.equalTo(50)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        nickSearchBar.snp.makeConstraints { make in
+            make.top.equalTo(passwordSearchBar.snp.bottom).offset(12)
+            make.height.equalTo(50)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        joinButton.snp.makeConstraints { make in
+            make.top.equalTo(nickSearchBar.snp.bottom).offset(12)
+            make.height.equalTo(50)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        bunDownImageView.snp.makeConstraints { make in
+            make.top.equalTo(joinButton.snp.bottom).offset(12)
+            make.height.equalTo(60)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(16)
+        }
+    }
+    
+    override func configureUI() {
+        navigationItem.title = "회원가입"
+        setupBackButton()
+        emailSearchBar.keyboardType = .emailAddress
+        passwordSearchBar.isSecureTextEntry = true
+    }
+}
+
+extension LFRBJoinViewController {
+    private func bind() {
+        emailSearchBar.rx.text.orEmpty
+            .bind(with: self) { owner, text in
+                print(text)
+                owner.viewModel.didEditEmailText(text: text)
+            }
+            .disposed(by: disposeBag)
+        
+        passwordSearchBar.rx.text.orEmpty
+            .bind(with: self) { owner, text in
+                owner.viewModel.didEditPasswordText(text: text)
+            }
+            .disposed(by: disposeBag)
+        
+        nickSearchBar.rx.text.orEmpty
+            .bind(with: self) { owner, text in
+                owner.viewModel.didEditNickText(text: text)
+            }
+            .disposed(by: disposeBag)
+        
+        emailValidButton.rx.tap
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .withLatestFrom(emailSearchBar.rx.text.orEmpty)
+            .bind(with: self) { owner, email in
+                owner.viewModel.didTapEmailValid(email: email)
+            }
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(
+            emailSearchBar.rx.text.orEmpty,
+            passwordSearchBar.rx.text.orEmpty,
+            nickSearchBar.rx.text.orEmpty,
+            viewModel.isNotDuplicateEmail
+        )
+        .bind(with: self) { owner, field in
+            print(field)
+            if !field.0.isEmpty, !field.1.isEmpty, !field.2.isEmpty, field.3 {
+                owner.viewModel.didFilledFieldData(isFilled: true)
+            } else {
+                owner.viewModel.didFilledFieldData(isFilled: false)
+            }
+        }
+        .disposed(by: disposeBag)
+        
+        let fieldData = Observable.zip(
+            emailSearchBar.rx.text.orEmpty,
+            passwordSearchBar.rx.text.orEmpty,
+            nickSearchBar.rx.text.orEmpty
+        )
+        joinButton.rx.tap
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .withLatestFrom(fieldData)
+            .map { JoinQuery(email: $0.0, password: $0.1, nick: $0.2) }
+            .bind(with: self) { owner, query in
+                owner.viewModel.didTapJoin(query: query)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.trimmedEmailText
+            .bind(to: emailSearchBar.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.trimmedPasswordText
+            .bind(to: passwordSearchBar.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.trimmedNickText
+            .bind(to: nickSearchBar.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.isValidEmail
+            .bind(with: self) { owner, isValid in
+                owner.emailValidImage.isHidden = !isValid
+                owner.emailValidButton.isEnabled = isValid
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.toastMessage
+            .asDriver(onErrorJustReturn: "")
+            .drive(with: self) { owner, message in
+                owner.view.makeToast(message, duration: 1.5, position: .center)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.isFilledFieldData
+            .bind(to: joinButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+    }
+}
