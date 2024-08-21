d24c0a60b2bf0f8d515a51d5cd18178a1654148b
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-21 23:34:09 +0900

    💾 Add: 회원가입 성공 시, 회원가입완료 화면으로 가도록 해당 화면 구성

---
+++ LookForRealBurger/Scene/Join/Presentation/JoinCompleteViewController.swift
@@ -0,0 +1,106 @@
+//
+//  JoinCompleteViewController.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/20/24.
+//
+
+import UIKit
+
+import Lottie
+import RxCocoa
+import RxSwift
+import SnapKit
+
+final class JoinCompleteViewController: BaseViewController {
+    private let congratulationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.Color.brown
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = R.Font.chab30
+        return label
+    }()
+    
+    private let burgerImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "cartoon_burger")
+        return imageView
+    }()
+    
+    private let goToLoginButton = PretendardRoundedButton(
+        title: "로그인 하러가기", backgroudColor: R.Color.red
+    )
+    
+    private var user: JoinUser!
+    private var disposeBag: DisposeBag!
+    
+    static func create(
+        user: JoinUser,
+        disposeBag: DisposeBag = DisposeBag()
+    ) -> JoinCompleteViewController {
+        let view = JoinCompleteViewController()
+        view.user = user
+        view.disposeBag = disposeBag
+        return view
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startFireworkAnimation()
+        
+        goToLoginButton.rx.tap
+            .bind(with: self) { owner, _ in
+                let loginViewController = LoginScene.makeView()
+                owner.changeRootViewController(loginViewController)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    override func configureNavigation() {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(congratulationLabel)
+        view.addSubview(burgerImage)
+        view.addSubview(goToLoginButton)
+    }
+    
+    override func configureLayout() {
+        congratulationLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.bottom.equalTo(burgerImage.snp.top).offset(-16)
+        }
+        
+        burgerImage.snp.makeConstraints { make in
+            make.center.equalTo(view.safeAreaLayoutGuide)
+            make.size.equalTo(200)
+        }
+        
+        goToLoginButton.snp.makeConstraints { make in
+            make.top.equalTo(burgerImage.snp.bottom).offset(16)
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.width.equalTo(200)
+            make.height.equalTo(44)
+        }
+    }
+    
+    override func configureUI() {
+        congratulationLabel.text = R.Phrase.getCongratulation(nick: user.nick)
+    }
+    
+    private func startFireworkAnimation() {
+        let fireworkAnimationView = LottieAnimationView(name: "fireworkAnimation")
+        view.addSubview(fireworkAnimationView)
+        fireworkAnimationView.frame = view.bounds
+        fireworkAnimationView.center = view.center
+        fireworkAnimationView.contentMode = .scaleAspectFit
+        fireworkAnimationView.loopMode = .playOnce
+        fireworkAnimationView.play { completed in
+            fireworkAnimationView.isHidden = completed
+        }
+    }
+}
