41953ac86efa70eec0af84c9ee4da15de38ecccf
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-22 14:36:26 +0900

    💾 Add: 백버튼 커스텀 시 팝제스쳐가 먹히지 않아 UINavigationController extension에서 팝제스쳐 추가

---
+++ LookForRealBurger/Common/Utilities/Extension/UINavigationController+.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/22/24.
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
