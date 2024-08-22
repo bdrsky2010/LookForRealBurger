19eb08c2f53fcb46b466c890b209f9dabb0a154e
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-22 14:39:06 +0900

    💾 Add: Login에 대한 로직을 담고 있는 useCase 추상화된 프로토콜 구성 및 프로토콜이 적용된 구조체 내에 로직 구성

--- LookForRealBurger/Scene/Login/Domain/UseCase/LoginUseCase.swift
+++ LookForRealBurger/Scene/Login/Domain/UseCase/LoginUseCase.swift
@@ -7,14 +7,36 @@
 
 import Foundation
 
+import RxSwift
+
 protocol LoginUseCase {
-    
+    func loginExecute(query: LoginQuery) -> Single<Result<LoginUser, LoginError>>
 }
 
 final class DefaultLoginUseCase {
+    private let loginService: LoginService
     
+    init(loginService: LoginService) {
+        self.loginService = loginService
+    }
 }
 
 extension DefaultLoginUseCase: LoginUseCase {
-    
+    func loginExecute(query: LoginQuery) -> Single<Result<LoginUser, LoginError>> {
+        Single.create { [weak self] single in
+            guard let self else {
+                single(.success(.failure(.unknown(R.Phrase.errorOccurred))))
+                return Disposables.create()
+            }
+            loginService.loginRequest(query: query) { result in
+                switch result {
+                case .success(let value):
+                    single(.success(.success(value)))
+                case .failure(let error):
+                    single(.success(.failure(error)))
+                }
+            }
+            return Disposables.create()
+        }
+    }
 }
