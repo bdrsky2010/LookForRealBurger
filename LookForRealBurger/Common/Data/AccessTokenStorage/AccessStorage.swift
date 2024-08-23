1b74c69e2c4a1df2883fb09be315deabc479456e
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-23 19:54:14 +0900

    💾 Add: 리프레시 토큰 만료 시,  자동 로그인을 위해 아이디, 비밀번호도 UserDefaults에 저장해주기 위해 추가 구성

---
+++ LookForRealBurger/Common/Data/AccessTokenStorage/AccessStorage.swift
@@ -0,0 +1,62 @@
+//
+//  AccessTokenStorage.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/15/24.
+//
+
+import Foundation
+
+protocol AccessStorage: AnyObject {
+    var accessToken: String { get set }
+    var refreshToken: String { get set }
+    var accessEmail: String { get set }
+    var accessPassword: String { get set }
+    
+    func removeToken()
+}
+
+final class UserDefaultsAccessStorage {
+    static let shared = UserDefaultsAccessStorage()
+    
+    private let accessTokenKey = "LookForRealBurgerAccess"
+    private let refreshTokenKey = "LookForRealBurgerRefresh"
+    private let accessEmailKey = "LookForRealBurgerEmail"
+    private let accessPasswordKey = "LookForRealBurgerPassword"
+    private let userDefaults: UserDefaults
+    
+    private init(
+        userDefaults: UserDefaults = UserDefaults.standard
+    ) {
+        self.userDefaults = userDefaults
+    }
+    
+    private func resetUserDefaults() {
+        for key in userDefaults.dictionaryRepresentation().keys {
+            userDefaults.removeObject(forKey: key.description)
+        }
+    }
+}
+
+extension UserDefaultsAccessStorage: AccessStorage {
+    var accessToken: String {
+        get { userDefaults.string(forKey: accessTokenKey) ?? "" }
+        set { userDefaults.set(newValue, forKey: accessTokenKey) }
+    }
+    var refreshToken: String {
+        get { userDefaults.string(forKey: refreshTokenKey) ?? "" }
+        set { userDefaults.set(newValue, forKey: refreshTokenKey) }
+    }
+    var accessEmail: String {
+        get { userDefaults.string(forKey: accessEmailKey) ?? "" }
+        set { userDefaults.set(newValue, forKey: accessEmailKey) }
+    }
+    var accessPassword: String {
+        get { userDefaults.string(forKey: accessPasswordKey) ?? "" }
+        set { userDefaults.set(newValue, forKey: accessPasswordKey) }
+    }
+    
+    func removeToken() {
+        resetUserDefaults()
+    }
+}
