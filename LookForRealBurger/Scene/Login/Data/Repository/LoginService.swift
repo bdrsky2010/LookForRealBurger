1372e90c51b906ebe994086a599a50d0e610618b
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-23 19:54:44 +0900

    🩹 Refactor: 이름 변경

--- LookForRealBurger/Scene/Login/Data/Repository/LoginService.swift
+++ LookForRealBurger/Scene/Login/Data/Repository/LoginService.swift
@@ -1,5 +1,5 @@
 //
-//  LoginService.swift
+//  LoginRepository.swift
 //  LookForRealBurger
 //
 //  Created by Minjae Kim on 8/21/24.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol LoginService {
+protocol LoginRepository {
     func loginRequest(
         query: LoginQuery,
         completion: @escaping (Result<LoginUser, LoginError>) -> Void
@@ -21,20 +21,20 @@ enum LoginError: Error {
     case unknown(_ message: String)
 }
 
-final class DefaultLoginService {
+final class DefaultLoginRepository {
     private let network: NetworkManager
-    private let tokenStorage: AccessTokenStorage
+    private let accessStorage: AccessStorage
     
     init(
         network: NetworkManager,
-        tokenStorage: AccessTokenStorage
+        accessStorage: AccessStorage
     ) {
         self.network = network
-        self.tokenStorage = tokenStorage
+        self.accessStorage = accessStorage
     }
 }
 
-extension DefaultLoginService: LoginService {
+extension DefaultLoginRepository: LoginRepository {
     func loginRequest(
         query: LoginQuery,
         completion: @escaping (Result<LoginUser, LoginError>
@@ -51,32 +51,34 @@ extension DefaultLoginService: LoginService {
             guard let self else { return }
             switch result {
             case .success(let success):
-                tokenStorage.accessToken = success.accessToken
-                tokenStorage.refreshToken = success.refreshToken
+                accessStorage.accessToken = success.accessToken
+                accessStorage.refreshToken = success.refreshToken
+                accessStorage.accessEmail = query.email
+                accessStorage.accessPassword = query.password
                 completion(.success(success.toDomain()))
             case .failure(let failure):
                 switch failure {
                 case .requestFailure(let error):
                     completion(.failure(.network("에러가 발생하였습니다")))
-                    print("LoginService 네트워크 에러 -> \(failure.self)")
+                    print("LoginRepository 네트워크 에러 -> \(failure.self)")
                     print(error.localizedDescription)
                 case .apiKey, .invalidData, .tooManyRequest, .invalidURL:
                     completion(.failure(.network("에러가 발생하였습니다")))
-                    print("LoginService 네트워크 에러 -> \(failure.self)")
+                    print("LoginRepository 네트워크 에러 -> \(failure.self)")
                 case .networkFailure:
                     completion(.failure(.network("인터넷 연결이 불안정합니다")))
-                    print("LoginService 네트워크 에러 -> networkFailure")
+                    print("LoginRepository 네트워크 에러 -> networkFailure")
                 case .unknown(let statusCode):
                     switch statusCode {
                     case 400:
                         completion(.failure(.missingFields("아이디, 비밀번호를 입력해주세요")))
-                        print("LoginService 로그인 에러 -> missingFields")
+                        print("LoginRepository 로그인 에러 -> missingFields")
                     case 401:
                         completion(.failure(.accountVerify("계정을 다시 확인해주세요")))
-                        print("LoginService 로그인 에러 -> accountVerify")
+                        print("LoginRepository 로그인 에러 -> accountVerify")
                     default:
                         completion(.failure(.unknown("알 수 없는 에러 발생")))
-                        print("LoginService 로그인 에러 -> unknown")
+                        print("LoginRepository 로그인 에러 -> unknown")
                     }
                 }
             }
