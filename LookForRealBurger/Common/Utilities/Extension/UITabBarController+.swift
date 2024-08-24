47553573b5a9e4be006cf44eb63c5c08d0ec269e
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-24 23:20:19 +0900

    💾 Add: 탭바 아이템중 리뷰를 작성 하는 탭에서 모달뷰의 x버튼 클릭 시 그 전에 화면에 보여지고 있던 탭으로 전환을 해주기 위한 extension
    - UITabBarController extension
    - 타입 저장 프로퍼티로 전에 선택된 인덱스를 저장하기 위한 프로퍼티 정의
    - 연산 프로퍼티로 위에서 정의한 타입 저장 프로퍼티를 리턴

---
+++ LookForRealBurger/Common/Utilities/Extension/UITabBarController+.swift
@@ -0,0 +1,16 @@
+//
+//  UITabBarController+.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/24/24.
+//
+
+import UIKit
+
+extension UITabBarController {
+    static var previousSelectedIndex = 0
+    
+    var previousSelectedIndex: Int {
+        Self.previousSelectedIndex
+    }
+}
