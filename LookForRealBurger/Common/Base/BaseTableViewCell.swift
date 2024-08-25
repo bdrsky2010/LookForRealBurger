4a4f217541d9a1462f0873d32e1e43b9611a394e
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-25 23:48:02 +0900

    💾 Add: BaseTableViewCell 구성

---
+++ LookForRealBurger/Common/Base/BaseTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BaseTableViewCell.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/25/24.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureUI() { }
+}
