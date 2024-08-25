85d1c24033cd15936408ae20b3fa4514a8cb4a40
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-25 23:53:17 +0900

    💾 Add: 앱 내에서 쿼리로 보낼때 사용하는 타입

---
+++ LookForRealBurger/Scene/WriteReview/Domain/Entities/LocalSearchQuery.swift
@@ -0,0 +1,15 @@
+//
+//  LocalSearchQuery.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/25/24.
+//
+
+import Foundation
+
+struct LocalSearchQuery {
+    let query: String
+    let x: Double
+    let y: Double
+    var page: Int
+}
