ffb3a70751fc4c529e365f2e29772fc177b01dd5
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-27 02:20:25 +0900

    💾 Add: 라운드 한 모양의 보더가 들어간 버튼 초기화구문 폰트 받는 걸루 하나 더 구성

--- LookForRealBurger/Common/Reusable/PretendardRoundedButton.swift
+++ LookForRealBurger/Common/Reusable/PretendardRoundedButton.swift
@@ -24,6 +24,22 @@ final class PretendardRoundedButton: UIButton {
         )
     }
     
+    init(title: String, font: UIFont, backgroudColor: UIColor) {
+        super.init(frame: .zero)
+        
+        configuration = .borderedProminent()
+        configuration?.baseBackgroundColor = backgroudColor
+        configuration?.attributedTitle = AttributedString(
+            NSAttributedString(
+                string: title,
+                attributes: [
+                    NSAttributedString.Key.font: font,
+                    NSAttributedString.Key.foregroundColor: R.Color.background
+                ]
+            )
+        )
+    }
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
