583a074c9e7cf8e9190d074e76e84266d687a3dd
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-27 04:30:38 +0900

    💾 Add: 햄버거 식당 서버에 업로드할 때 도메인에서 보내줄 쿼리 타입 정의

---
+++ LookForRealBurger/Scene/WriteReview/Domain/Entities/UploadBurgerHouseQuery.swift
@@ -0,0 +1,20 @@
+//
+//  UploadBurgerHouseQuery.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/27/24.
+//
+
+import Foundation
+
+struct UploadBurgerHouseQuery {
+    let name: String
+    let totalRating: Int
+    let hashtagName: String
+    let longitude: String
+    let latitude: String
+    let roadAddress: String
+    let phone: String
+    let localId: String
+    let productId: String
+}
