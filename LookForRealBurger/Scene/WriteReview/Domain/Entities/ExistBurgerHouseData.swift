f38f4b3816fa0ab74ef404f09f9455a615fc5e97
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-27 02:22:21 +0900

    💾 Add: 앱 내에서 사용되는 서버에 햄버거 식당 데이터가 존재하는지 유무를 알려줄 떄 사용되는 타입

---
+++ LookForRealBurger/Scene/WriteReview/Domain/Entities/ExistBurgerHouseData.swift
@@ -0,0 +1,12 @@
+//
+//  ExistBurgerHouseData.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/27/24.
+//
+
+import Foundation
+
+struct ExistBurgerHouseData {
+    let isExist: Bool
+}
