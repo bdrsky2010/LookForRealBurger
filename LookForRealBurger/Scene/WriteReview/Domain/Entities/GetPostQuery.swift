16690cd65dc5517a30429b81314ed729407dbb61
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-27 02:22:48 +0900

    💾 Add: 전체 포스트 조회할 때 사용할 쿼리 타입 정의

---
+++ LookForRealBurger/Scene/WriteReview/Domain/Entities/GetPostQuery.swift
@@ -0,0 +1,14 @@
+//
+//  GetPostQuery.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/27/24.
+//
+
+import Foundation
+
+struct GetPostQuery {
+    let next: String?
+    let limit: String
+    let productId: String
+}
