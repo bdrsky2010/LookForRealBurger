01d166b829e234df62682bcc652113493b4213eb
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-26 20:34:00 +0900

    💾 Add: UIWindowScene extension 내부에 기기 width 값과 width 값을 통해 화면 사이즈가 작은 기기인지 판단하는 타입 연산 프로퍼티 추가

---
+++ LookForRealBurger/Common/Utilities/Extension/UIWindowScene+.swift
@@ -0,0 +1,27 @@
+//
+//  UIWindowScene+.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/26/24.
+//
+
+import UIKit
+
+extension UIWindowScene {
+    static var deviceWidth: CGFloat? {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            let width = windowScene.screen.bounds.width
+            return width
+        } else {
+            return nil
+        }
+    }
+    
+    static var isSmallDevice: Bool {
+        if let deviceWidth, deviceWidth <= 375 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
