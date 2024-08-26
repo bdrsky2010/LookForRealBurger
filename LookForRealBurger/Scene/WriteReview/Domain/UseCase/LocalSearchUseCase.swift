028f08799a39430abbffa60f5a00481a594755e4
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-27 02:24:55 +0900

    💾 Add: 햄버거 식당 검색 로직을 담당하는 모듈에서 선택된 식당이 서버 api 존재하는지 유무를 판단하기 위한 모듈 및 메서드 구성

--- LookForRealBurger/Scene/WriteReview/Domain/UseCase/LocalSearchUseCase.swift
+++ LookForRealBurger/Scene/WriteReview/Domain/UseCase/LocalSearchUseCase.swift
@@ -10,28 +10,64 @@ import Foundation
 import RxSwift
 
 protocol LocalSearchUseCase {
-    func localSearchExecute(query: LocalSearchQuery) -> Single<Result<BurgerPage, KakaoAPIError>>
+    func localSearchExecute(
+        query: LocalSearchQuery
+    ) -> Single<Result<BurgerPage, KakaoAPIError>>
+    
+    func existBurgerHouseExecute(
+        query: GetPostQuery,
+        localId: String
+    ) -> Single<Result<ExistBurgerHouseData, GetPostError>>
 }
 
 final class DefaultLocalSearchUseCase {
     private let localSearchRepository: LocalSearchRepository
+    private let getPostRepository: GetPostRepository
     
-    init(localSearchRepository: LocalSearchRepository) {
+    init(
+        localSearchRepository: LocalSearchRepository,
+        getPostRepository: GetPostRepository
+    ) {
         self.localSearchRepository = localSearchRepository
+        self.getPostRepository = getPostRepository
     }
 }
 
 extension DefaultLocalSearchUseCase: LocalSearchUseCase {
-    func localSearchExecute(query: LocalSearchQuery) -> Single<Result<BurgerPage, KakaoAPIError>> {
-        Single.create { [weak self] single in
+    func localSearchExecute(
+        query: LocalSearchQuery
+    ) -> Single<Result<BurgerPage, KakaoAPIError>> {
+        return Single.create { [weak self] single in
             guard let self else {
                 single(.success(.failure(.unknown(message: R.Phrase.errorOccurred))))
                 return Disposables.create()
             }
-            localSearchRepository.Request(query: query) { result in
+            localSearchRepository.request(query: query) { result in
                 switch result {
                 case .success(let value):
                     single(.success(.success(value)))
+                case .failure(let error):
+                    single(.success(.failure(error)))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func existBurgerHouseExecute(
+        query: GetPostQuery,
+        localId: String
+    ) -> Single<Result<ExistBurgerHouseData, GetPostError>> {
+        return Single.create { [weak self] single in
+            guard let self else {
+                single(.success(.failure(.unknown(message: R.Phrase.errorOccurred))))
+                return Disposables.create()
+            }
+            getPostRepository.getPostRequest(query: query) { result in
+                switch result {
+                case .success(let value):
+                    let isExist = !value.filter { $0.localId == localId }.isEmpty
+                    single(.success(.success(.init(isExist: isExist))))
                 case .failure(let error):
                     single(.success(.failure(error)))
                 }
