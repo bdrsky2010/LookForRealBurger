5294ebf640aaec0c5dd22abc471483582dbbf1f6
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-27 02:19:51 +0900

    💾 Add: API 조회 시 사용할 product_id 리터럴 값을 열거형으로 구성

---
+++ LookForRealBurger/Common/Data/Network/LFRBProductID.swift
@@ -0,0 +1,13 @@
+//
+//  LFRBProductID.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/26/24.
+//
+
+import Foundation
+
+enum LFRBProductID: String {
+    case burgerHouse = "LFRB_BURGER_HOUSE"
+    case review      = "LFRB_BURGER_HOUSE_REVIEW"
+}
