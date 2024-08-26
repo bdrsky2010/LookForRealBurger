7af34f8e31b513e1f1177f2ed5da362c9755c21a
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-26 20:44:38 +0900

    💾 Add: 햄버거집 리뷰 작성 페이지 구성중
    # 현재까지 완료된 상황
    - 뷰는 대충 구성 해놓음
    - 장소 검색 텍스트필드 탭해서 장소 검색화면전환
    - 장소 검색 화면에서 아이템 선택하면 다시 전 화면으로 화면전환해주며 선택된 아이템 데이터 가져옴
    - 사진 추가 버튼 탭하면 촬영, 앨범 선택하게 알럿 액션시트 보여주며
    - 사진 추가 시, 용량 5MB 넘어가면 에러문구 토스트로 띄워줌
    - 저장 버튼 눌렀을 때 일단은 사진 업로드 테스트 완료
    
    # 미완료 상황
    - 제목, 내용, 평점, 사진(1개라도 존재하면) 다 작성되었을 때 저장 버튼 활성화시키도록 변경 필요
    - 게시글 저장 필요
    - 액세스 토큰 만료 시, 리프레시
    - 리프레시 토큰 만료시, 다시 자동 로그인 되도록 모듈 구성 필요
    - 저장 완료 시, 리뷰탭으로 이동 필요

---
+++ LookForRealBurger/Scene/WriteReview/Presentation/WriteReviewViewModel.swift
@@ -0,0 +1,130 @@
+//
+//  WriteReviewViewModel.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/25/24.
+//
+
+import Foundation
+
+import RxCocoa
+import RxSwift
+
+protocol WriteReviewInput {
+    func xButtonTap()
+    func searchBarTap()
+    func burgerHouseSelect(burgerHouse: BurgerHouse)
+    func imageTap()
+    func cameraSelect()
+    func gallerySelect()
+    func saveTap(files: [Data])
+    func imageUploadSuccess(uploadedImage: UploadedImage)
+}
+
+protocol WriteReviewOutput {
+    var goToPreviousTab: PublishRelay<Void> { get }
+    var goToLocalSearch: PublishRelay<Void> { get }
+    var selectedBurgerHouse: PublishRelay<String> { get }
+    var presentAddPhotoAction: PublishRelay<Void> { get }
+    var presentCamera: PublishRelay<Void> { get }
+    var presentGallery: PublishRelay<Void> { get }
+    var toastMessage: PublishRelay<String> { get }
+    var uploadSuccess: PublishRelay<UploadedImage> { get }
+}
+
+typealias WriteReviewViewModel = WriteReviewInput & WriteReviewOutput
+
+final class DefaultWriteReviewViewModel: WriteReviewOutput {
+    var goToPreviousTab = PublishRelay<Void>()
+    var goToLocalSearch = PublishRelay<Void>()
+    var selectedBurgerHouse = PublishRelay<String>()
+    var presentAddPhotoAction = PublishRelay<Void>()
+    var presentCamera = PublishRelay<Void>()
+    var presentGallery = PublishRelay<Void>()
+    var toastMessage = PublishRelay<String>()
+    var uploadSuccess = PublishRelay<UploadedImage>()
+    
+    private var burgerHouse: BurgerHouse?
+    private var loginUseCase: LoginUseCase!
+    private var postUploadUseCase: PostUploadUseCase!
+    private var disposeBag: DisposeBag!
+    
+    init(
+        loginUseCase: LoginUseCase,
+        postUploadUseCase: PostUploadUseCase,
+        disposeBag: DisposeBag = DisposeBag()
+    ) {
+        self.loginUseCase = loginUseCase
+        self.postUploadUseCase = postUploadUseCase
+        self.disposeBag = disposeBag
+    }
+}
+
+extension DefaultWriteReviewViewModel: WriteReviewInput {
+    func xButtonTap() {
+        goToPreviousTab.accept(())
+    }
+    
+    func searchBarTap() {
+        goToLocalSearch.accept(())
+    }
+    
+    func burgerHouseSelect(burgerHouse: BurgerHouse) {
+        Observable.just(burgerHouse.name)
+            .bind(onNext: selectedBurgerHouse.accept(_:))
+            .disposed(by: disposeBag)
+    }
+    
+    func imageTap() {
+        presentAddPhotoAction.accept(())
+    }
+    
+    func cameraSelect() {
+        presentCamera.accept(())
+    }
+    
+    func gallerySelect() {
+        presentGallery.accept(())
+    }
+    
+    func saveTap(files: [Data]) {
+        guard files.reduce(0, { $0 + $1.count }) < 5000000 else {
+            toastMessage.accept("사진 용량 제한: 5MB 제한")
+            return
+        }
+        
+        postUploadUseCase.uploadImage(files: files)
+            .asDriver(onErrorJustReturn: .failure(.unknown(message: R.Phrase.errorOccurred)))
+            .drive(with: self) { owner, result in
+                switch result {
+                case .success(let value):
+                    owner.imageUploadSuccess(uploadedImage: value)
+                case .failure(let error):
+                    switch error {
+                    case .network(let message):
+                        owner.toastMessage.accept(message)
+                    case .badRequest(let message):
+                        owner.toastMessage.accept(message)
+                    case .invalidToken(let message):
+                        owner.toastMessage.accept(message)
+                    case .forbidden(let message):
+                        owner.toastMessage.accept(message)
+                    case .expiredToken:
+                        // TODO: 토큰 리프레시
+                        print("토큰 리프레시 필요")
+                    case .unknown(let message):
+                        owner.toastMessage.accept(message)
+                    }
+                }
+            } onCompleted: { _ in
+                print("uploadImage completed")
+            } onDisposed: { _ in
+                print("uploadImage disposed")
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func imageUploadSuccess(uploadedImage: UploadedImage) {
+        print(#function, uploadedImage)
+    }
+}
