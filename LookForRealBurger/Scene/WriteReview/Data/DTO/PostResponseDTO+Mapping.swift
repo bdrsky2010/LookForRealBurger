4e85ca6be7f28c63709cb1d39874fb8989fd482b
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-27 23:33:03 +0900

    🩹 Refactor: Post에 대한 응답 객체로 데이터를 받을 때 요청 객체에서 데이터를 보내주지 않은 데이터에 대해서는 아예 보내주지 않아 옵셔널로 타입을 변경했고 그에따른 수정

--- LookForRealBurger/Scene/WriteReview/Data/DTO/PostResponseDTO+Mapping.swift
+++ LookForRealBurger/Scene/WriteReview/Data/DTO/PostResponseDTO+Mapping.swift
@@ -14,15 +14,16 @@ struct PostResponseDTO: Decodable {
     let price: Int
     let content: String
     let content1: String
-    let content2: String
-    let content3: String
-    let content4: String
-    let content5: String
+    let content2: String?
+    let content3: String?
+    let content4: String?
+    let content5: String?
     let createdAt: String
     let creator: CreatorResponseDTO
     let files: [String]
     let likes: [String]
     let likes2: [String]
+    let buyers: [String]
     let hashTags: [String]
     let comments: [CommentResponseDTO]
     
@@ -42,6 +43,7 @@ struct PostResponseDTO: Decodable {
         case files
         case likes
         case likes2
+        case buyers
         case hashTags
         case comments
     }
@@ -55,15 +57,39 @@ extension PostResponseDTO {
             totalRating: self.price,
             hashtagName: self.content,
             longitude: self.content1,
-            latitude: self.content2,
-            roadAddress: self.content3,
-            phone: self.content4,
-            localId: self.content5,
+            latitude: self.content2 ?? "",
+            roadAddress: self.content3 ?? "",
+            phone: self.content4 ?? "",
+            localId: self.content5 ?? "",
             productId: self.productId,
             eatenUserIds: self.likes,
             plannedUserIds: self.likes2,
             hashTags: self.hashTags,
             reviewIds: self.comments.map { $0.content }
+        )
+    }
+}
+
+extension PostResponseDTO {
+    func toDomain() -> BurgerHouseReview {
+        return BurgerHouseReview(
+            reviewId: self.postId,
+            title: self.title,
+            rating: self.price,
+            content: self.content,
+            burgerHousePostId: self.content1,
+            createdAt: self.createdAt,
+            creator: .init(userId: self.creator.userId, nick: self.creator.nick),
+            files: self.files,
+            likeUserIds: self.likes,
+            bookmarkUserIds: self.likes2,
+            comments: self.comments.map {
+                Comment(
+                    commentId: $0.commentId,
+                    content: $0.content,
+                    createdAt: $0.createdAt,
+                    creator: .init(userId: $0.creator.userId, nick: $0.creator.nick))
+            }
         )
     }
 }
