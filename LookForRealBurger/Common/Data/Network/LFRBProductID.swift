5e5da185fbd78edbb4af294295c5f3c34f24e0d6
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-27 23:30:27 +0900

    💾 Add: 테스트용 ProductID 구성

--- LookForRealBurger/Common/Data/Network/LFRBProductID.swift
+++ LookForRealBurger/Common/Data/Network/LFRBProductID.swift
@@ -10,4 +10,6 @@ import Foundation
 enum LFRBProductID: String {
     case burgerHouse = "LFRB_BURGER_HOUSE"
     case review      = "LFRB_BURGER_HOUSE_REVIEW"
+    case burgerHouseTest = "LFRB_BURGER_HOUSE_TEST"
+    case reviewTest      = "LFRB_BURGER_HOUSE_REVIEW_TEST"
 }
