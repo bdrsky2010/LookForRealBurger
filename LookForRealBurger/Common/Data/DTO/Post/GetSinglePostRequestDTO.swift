d2449c903cc7d05566690cbf248e6d4ff3339f45
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-28 23:29:59 +0900

    💾 Add: 특정 단일 포스트 요청 시 사용되는 DTO 모델

---
+++ LookForRealBurger/Common/Data/DTO/Post/GetSinglePostRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  GetSinglePostRequestDTO.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/28/24.
+//
+
+import Foundation
+
+struct GetSinglePostRequestDTO {
+    let postId: String
+}
