a50bcba00d53c365f7e025d947eac1281031bcec
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-28 23:37:18 +0900

    💾 Add: 단일 포스트 요청 시 사용될 쿼리 모델

---
+++ LookForRealBurger/Scene/BurgerMap/Domain/Entities/GetSinglePostQuery.swift
@@ -0,0 +1,12 @@
+//
+//  GetSinglePostQuery.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/28/24.
+//
+
+import Foundation
+
+struct GetSinglePostQuery {
+    let postId: String
+}
