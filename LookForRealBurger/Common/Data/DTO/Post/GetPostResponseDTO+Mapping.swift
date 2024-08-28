35f6c10d6484e8149f08ff55a7fd8986b1125681
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-28 23:29:34 +0900

    💾 Add:  포스트 조회 API DTO Mapping 메서드 추가 및 수정
    - 포스트 조회 API 에 사용되는 데이터 전송 객체에서 버거맵 화면에서 사용할 데이터로 가공하는 메서드 추가
    - 햄버거집 포스트 업로드 시 받아올 떄 가공하는 데이터 타입에서 totalRating이 사용이 어려워져 삭제하여 코드 수정

--- LookForRealBurger/Common/Data/DTO/Post/GetPostResponseDTO+Mapping.swift
+++ LookForRealBurger/Common/Data/DTO/Post/GetPostResponseDTO+Mapping.swift
@@ -23,7 +23,6 @@ extension GetPostResponseDTO {
             return GetBurgerHouse(
                 burgerHousePostId: $0.postId,
                 name: $0.title,
-                totalRating: $0.price,
                 hashtagName: $0.content,
                 longitude: $0.content1,
                 latitude: $0.content2 ?? "",
@@ -36,6 +35,37 @@ extension GetPostResponseDTO {
                 hashTags: $0.hashTags,
                 reviewIds: $0.comments.map { $0.content }
             )
+        }
+    }
+    
+    // 지도에서 사용할 데이터
+    func toDomain() -> [BurgerMapHouse] {
+        return self.data.compactMap {
+            if let longitude = Double($0.content1),
+               let content2 = $0.content2,
+               let latitude = Double(content2),
+               let roadAddress = $0.content3,
+               let phone = $0.content4,
+               let localId = $0.content5 {
+                return BurgerMapHouse(
+                    burgerHousePostId: $0.postId,
+                    name: $0.title,
+                    hashtagName: $0.content,
+                    longitude: longitude,
+                    latitude: latitude,
+                    roadAddress: roadAddress,
+                    phone: phone,
+                    localId: localId,
+                    productId: LFRBProductID.burgerHouseTest.rawValue,
+                    eatenUserIds: $0.likes,
+                    plannedUserIds: $0.likes2,
+                    hashTags: $0.hashTags,
+                    reviewIds: $0.comments.map { $0.content },
+                    reviews: []
+                )
+            } else {
+                return nil
+            }
         }
     }
 }
