1adb2a3d60447e8c13e8fdadf7509cf321122f6f
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-30 23:24:38 +0900

    🩹 Refactor: 리뷰 탭에서 사용되는 리뷰용 이미지 셀 라운드가 안들어가있음

---
+++ LookForRealBurger/Common/Reusable/Cell/BurgerReviewImageCell.swift
@@ -0,0 +1,55 @@
+//
+//  BurgerReviewImageCell.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 8/30/24.
+//
+
+import UIKit
+
+import RxSwift
+import SnapKit
+
+final class BurgerReviewImageCell: BaseCollectionViewCell {
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let multipleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "square.fill.on.square.fill")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        imageView.preferredSymbolConfiguration = .init(font: .systemFont(ofSize: 14))
+        imageView.isHidden = true
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    var disposeBag = DisposeBag()
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func configureHierarchy() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(multipleImageView)
+    }
+    
+    override func configureLayout() {
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        multipleImageView.snp.makeConstraints { make in
+            make.top.trailing.equalTo(imageView).inset(8)
+        }
+    }
+}
