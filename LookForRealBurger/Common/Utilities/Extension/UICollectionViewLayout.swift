fa5bf18e66fe6a25eb8a0ad6c092a87b4c6daa1d
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-08-30 23:26:04 +0900

    💾 Add: 컴포지셔널 레이아웃 구성 메서드 추가

--- LookForRealBurger/Common/Utilities/Extension/UICollectionViewLayout.swift
+++ LookForRealBurger/Common/Utilities/Extension/UICollectionViewLayout.swift
@@ -14,14 +14,31 @@ extension UICollectionViewLayout {
             heightDimension: .fractionalHeight(1.0)
         )
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.85),
+            heightDimension: .fractionalWidth(0.6)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+        section.interGroupSpacing = 20
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    static func createReviewImageLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
         let groupSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1.0),
             heightDimension: .fractionalWidth(1.0)
         )
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         let section = NSCollectionLayoutSection(group: group)
-        section.interGroupSpacing = 10
         section.orthogonalScrollingBehavior = .groupPagingCentered
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
