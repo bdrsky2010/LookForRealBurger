4e81eb531eb08d6ee2091ca0d80fed8246a91797
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-02 07:47:11 +0900

    🩹 Refactor: 오타 수정

--- LookForRealBurger/Common/Data/DTO/Follow/FollowResponseDTO+Mapping.swift
+++ LookForRealBurger/Common/Data/DTO/Follow/FollowResponseDTO+Mapping.swift
@@ -10,11 +10,17 @@ import Foundation
 struct FollowResponseDTO: Decodable {
     let nick: String
     let opponentNick: String
-    let followingStatust: Bool
+    let followingStatus: Bool
     
     enum CodingKeys: String, CodingKey {
         case nick
         case opponentNick = "opponent_nick"
-        case followingStatust = "following_status"
+        case followingStatus = "following_status"
+    }
+}
+
+extension FollowResponseDTO {
+    func toDomain() -> GetFollowStatus {
+        return GetFollowStatus(followingStatus: self.followingStatus)
     }
 }
