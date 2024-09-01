a5cbe5c3432d51efdd97538ff733058514b9b358
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-01 23:43:22 +0900

    🩹 Refactor: 좋아요 라우터, 팔로우 라우터 구성

--- LookForRealBurger/Common/Data/Network/Router/FollowRouter.swift
+++ LookForRealBurger/Common/Data/Network/Router/FollowRouter.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+import Moya
+
+enum FollowRouter {
+    case follow(_ userId: String)
+    case cancelFollow(_ userId: String)
+}
+
+extension FollowRouter: LFRBTargetType {
+    var path: String {
+        switch self {
+        case .follow(let userId), .cancelFollow(let userId): 
+            return "v1/follow/\(userId)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .follow:       return .post
+        case .cancelFollow: return .delete
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .follow, .cancelFollow: 
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .follow, .cancelFollow:
+            return [
+                LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
+                LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
+            ]
+        }
+    }
+}
--- LookForRealBurger/Common/Data/Network/Router/LikeRouter.swift
+++ LookForRealBurger/Common/Data/Network/Router/LikeRouter.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+import Moya
+
+enum LikeRouter {
+    case like(_ postId: String, _ dto: LikeRequestDTO)
+    case like2(_ postId: String, _ dto: LikeRequestDTO)
+    case myLikePost(_ dto: GetPostRequestDTO)
+    case myLikePost2(_ dto: GetPostRequestDTO)
+}
+
+extension LikeRouter: LFRBTargetType {
+    var path: String {
+        switch self {
+        case .like(let postId, _):  return "v1/posts/\(postId)/like"
+        case .like2(let postId, _): return "v1/posts/\(postId)/like-2"
+        case .myLikePost:           return "v1/posts/likes/me"
+        case .myLikePost2:          return "v1/posts/likes-2/me"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .like, .like2:             return .post
+        case .myLikePost, .myLikePost2: return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .like(_, let dto), .like2(_, let dto):
+            return .requestParameters(parameters: dto.asParameters, encoding: JSONEncoding.default)
+        case .myLikePost(let dto), .myLikePost2(let dto):
+            let parameters = dto.asParameters
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .like, .like2:
+            return [
+                LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
+                LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
+            ]
+        case .myLikePost, .myLikePost2:
+            return [
+                LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
+                LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
+            ]
+        }
+    }
+}
