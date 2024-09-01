aa6f168a3c371e13c2b6c72368f583ca7bcf0a45
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-01 23:46:35 +0900

    💾 Add: 캡슐모양 버튼 이니셜라이저 추가 구성

--- LookForRealBurger/Common/Reusable/CapsuleButton.swift
+++ LookForRealBurger/Common/Reusable/CapsuleButton.swift
@@ -25,6 +25,23 @@ final class CapsuleButton: UIButton {
         )
     }
     
+    init(title: String, font: UIFont, backgroudColor: UIColor) {
+        super.init(frame: .zero)
+        
+        configuration = .bordered()
+        configuration?.cornerStyle = .capsule
+        configuration?.baseBackgroundColor = backgroudColor
+        configuration?.attributedTitle = AttributedString(
+            NSAttributedString(
+                string: title,
+                attributes: [
+                    NSAttributedString.Key.font: font,
+                    NSAttributedString.Key.foregroundColor: R.Color.background
+                ]
+            )
+        )
+    }
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
