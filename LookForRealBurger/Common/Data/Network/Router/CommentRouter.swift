5ee93381fd0617537a6b0a3c193a6111294c129a
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-01 23:43:04 +0900

    🩹 Refactor: 댓글 라우터 케이스 추가

--- LookForRealBurger/Common/Data/Network/Router/CommentRouter.swift
+++ LookForRealBurger/Common/Data/Network/Router/CommentRouter.swift
@@ -11,18 +11,24 @@ import Moya
 
 enum CommentRouter {
     case comment(_ postId: String, _ dto: CommentRequestDTO)
+    case updateComment(_ postId: String, _ commentId: String, _ dto: CommentRequestDTO)
+    case deleteComment(_ postId: String, _ commentId: String)
 }
 
 extension CommentRouter: LFRBTargetType {
     var path: String {
         switch self {
-        case .comment(let postId, _): return "v1/posts/\(postId)/comments"
+        case .comment(let postId, _):                      return "v1/posts/\(postId)/comments"
+        case .updateComment(let postID, let commentId, _): return "v1/posts/\(postID)/comments/\(commentId)"
+        case .deleteComment(let postID, let commentId): return "v1/posts/\(postID)/comments/\(commentId)"
         }
     }
     
     var method: Moya.Method {
         switch self {
-        case .comment: return .post
+        case .comment:       return .post
+        case .updateComment: return .put
+        case .deleteComment: return .delete
         }
     }
     
@@ -33,6 +39,11 @@ extension CommentRouter: LFRBTargetType {
         case .comment(_, let dto):
             parameters = dto.asParameters
             return .requestParameters(parameters: parameters, encoding: JSONEncoding.default)
+        case .updateComment(_, _, let dto):
+            parameters = dto.asParameters
+            return .requestParameters(parameters: parameters, encoding: JSONEncoding.default)
+        case .deleteComment:
+            return .requestPlain
         }
     }
     
@@ -42,6 +53,17 @@ extension CommentRouter: LFRBTargetType {
             return [
                 LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
                 LFRBHeader.contentType.rawValue: LFRBHeader.json.rawValue,
+                LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
+            ]
+        case .updateComment:
+            return [
+                LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
+                LFRBHeader.contentType.rawValue: LFRBHeader.json.rawValue,
+                LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
+            ]
+        case .deleteComment:
+            return [
+                LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
                 LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
             ]
         }
