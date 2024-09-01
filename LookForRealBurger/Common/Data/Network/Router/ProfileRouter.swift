68e53f978e9c604e29f904de572f2a6074048d33
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-01 23:43:31 +0900

    🩹 Refactor: 프로필 라우터 구성

--- LookForRealBurger/Common/Data/Network/Router/ProfileRouter.swift
+++ LookForRealBurger/Common/Data/Network/Router/ProfileRouter.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+
+import Moya
+
+enum ProfileRouter {
+    case myProfile
+    case updateMyProfile(_ dto: UpdateProfileRequestDTO)
+    case otherProfile(_ userId: String)
+}
+
+extension ProfileRouter: LFRBTargetType {
+    var path: String {
+        switch self {
+        case .myProfile:                return "v1/users/me/profile"
+        case .updateMyProfile:          return "v1/users/me/profile"
+        case .otherProfile(let userId): return "v1/users/\(userId)/profile"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .myProfile:       return .get
+        case .updateMyProfile: return .put
+        case .otherProfile:    return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .myProfile:
+            return .requestPlain
+        case .updateMyProfile(let dto):
+            let parameters = dto.asParameters
+            return .requestParameters(parameters: parameters, encoding: JSONEncoding.default)
+        case .otherProfile:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .myProfile:
+            return [
+                LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
+                LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
+            ]
+        case .updateMyProfile:
+            return [
+                LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
+                LFRBHeader.contentType.rawValue: LFRBHeader.multipart.rawValue,
+                LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
+            ]
+        case .otherProfile:
+            return [
+                LFRBHeader.authorization.rawValue: UserDefaultsAccessStorage.shared.accessToken,
+                LFRBHeader.sesacKey.rawValue: APIKEY.lslp.rawValue
+            ]
+        }
+    }
+}
