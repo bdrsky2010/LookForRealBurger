517e7f7144b4d6b7cd250e4e83888e640dabc125
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-09-01 23:45:40 +0900

    💾 Add: 프로필 통신모듈 구성

--- LookForRealBurger/Common/Data/Repository/ProfileRepository.swift
+++ LookForRealBurger/Common/Data/Repository/ProfileRepository.swift
@@ -6,3 +6,123 @@
 //
 
 import Foundation
+
+enum ProfileError: Error {
+    // 공통
+    case network(_ message: String)
+    case invalidToken(_ message: String)
+    case forbidden(_ message: String)
+    case expiredToken
+    case unknown(_ message: String)
+}
+
+enum ProfileAPIType: String {
+    case profile
+}
+
+protocol ProfileRepository {
+    func myUserIdRequest(completion: @escaping (Result<GetMyUserId, ProfileError>) -> Void)
+    func myProfileRequest(completion: @escaping (Result<GetProfile, ProfileError>) -> Void)
+    func getOtherProfileRequest(
+        query: GetOtherProfileQuery,
+        completion: @escaping (Result<GetProfile, ProfileError>) -> Void
+    )
+}
+
+final class DefualtProfileRepository {
+    static let shared = DefualtProfileRepository()
+    
+    private let network: NetworkManager
+    
+    private init(
+        network: NetworkManager = LFRBNetworkManager.shared
+    ) {
+        self.network = network
+    }
+}
+
+extension DefualtProfileRepository: ProfileRepository {
+    func myUserIdRequest(completion: @escaping (Result<GetMyUserId, ProfileError>) -> Void) {
+        network.request(
+            ProfileRouter.myProfile,
+            of: ProfileResponseDTO.self
+        ) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let success):
+                completion(.success(success.toDomain()))
+            case .failure(let failure):
+                let profileError = errorHandling(type: .profile, failure: failure)
+                completion(.failure(profileError))
+            }
+        }
+    }
+    
+    func myProfileRequest(completion: @escaping (Result<GetProfile, ProfileError>) -> Void) {
+        network.request(
+            ProfileRouter.myProfile,
+            of: ProfileResponseDTO.self
+        ) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let success):
+                completion(.success(success.toDomain()))
+            case .failure(let failure):
+                let profileError = errorHandling(type: .profile, failure: failure)
+                completion(.failure(profileError))
+            }
+        }
+    }
+    
+    func getOtherProfileRequest(
+        query: GetOtherProfileQuery,
+        completion: @escaping (Result<GetProfile, ProfileError>) -> Void
+    ) {
+        network.request(
+            ProfileRouter.otherProfile(query.userId),
+            of: ProfileResponseDTO.self
+        ) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let success):
+                completion(.success(success.toDomain()))
+            case .failure(let failure):
+                let profileError = errorHandling(type: .profile, failure: failure)
+                completion(.failure(profileError))
+            }
+        }
+    }
+}
+
+extension DefualtProfileRepository {
+    private func errorHandling(
+        type: ProfileAPIType,
+        failure: NetworkError
+    ) -> ProfileError {
+        let profileError: ProfileError
+        switch failure {
+        case .requestFailure(let error):
+            profileError = .network(R.Phrase.errorOccurred)
+            print("ProfileRepository \(type.rawValue) network 에러 발생 -> \(error)")
+        case .apiKey, .invalidData, .tooManyRequest, .invalidURL:
+            profileError = .network(R.Phrase.errorOccurred)
+            print("ProfileRepository \(type.rawValue) network 에러 발생 -> \(failure)")
+        case .networkFailure:
+            profileError = .network(R.Phrase.errorOccurred)
+            print("ProfileRepository \(type.rawValue) network 에러 발생 -> \(failure)")
+        case .unknown(let statusCode):
+            switch statusCode {
+            case 401:
+                profileError = .invalidToken(R.Phrase.errorOccurred)
+            case 403:
+                profileError = .forbidden(R.Phrase.errorOccurred)
+            case 419:
+                profileError = .expiredToken
+            default:
+                profileError = .unknown("알 수 없는 에러 발생")
+            }
+        }
+        print("ProfileRepository \(type.rawValue) 에러 -> \(profileError)")
+        return profileError
+    }
+}
