db3e0c9a98bcd20bc772cf8472b62f3146a88f55
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2025-01-06 18:56:38 +0900

    refactor: 백그라운드 스레드 비동기 처리, 에러 핸들링 구현
    - 사용성 개선을 위해 Token 관련 메서드 접근 시, 백그라운드 처리

--- LookForRealBurger/Common/Secure/SecureTokenManager.swift
+++ LookForRealBurger/Common/Secure/SecureTokenManager.swift
@@ -7,10 +7,14 @@
 
 import Foundation
 
+enum SecureError: Error {
+    case error
+}
+
 protocol SecureTokenManager {
-    func encryptAndStoreToken(mockToken: String) -> Bool
-    func retrieveAndDecryptToken() -> String?
-    func deleteToken()
+    func encryptAndStoreToken(token: String, completion: @escaping (Result<Void, SecureError>) -> Void)
+    func retrieveAndDecryptToken(completion: @escaping (Result<String, SecureError>) -> Void)
+    func deleteToken(completion: @escaping (Result<Void, SecureError>) -> Void)
 }
 
 final class DefaultSecureTokenManager: SecureTokenManager {
@@ -30,34 +34,130 @@ final class DefaultSecureTokenManager: SecureTokenManager {
         self.keychainService = keychainService
     }
     
-    func encryptAndStoreToken(mockToken: String) -> Bool {
-        guard let tokenData = mockToken.data(using: .utf8),
-              let encryptedData = secureEnclaveService.encryptData(data: tokenData) else {
-            print("❌ 토큰 암호화 실패")
-            return false
+    func encryptAndStoreToken(token: String, completion: @escaping (Result<Void, SecureError>) -> Void) {
+        DispatchQueue.global().async { [weak self] in
+            guard let self else { return }
+            
+            guard let tokenData = token.data(using: .utf8),
+                  let encryptedData = secureEnclaveService.encryptData(data: tokenData) else {
+                print("❌ 토큰 암호화 실패")
+                completion(.failure(.error))
+                return
+            }
+            
+            keychainService.storeData(encryptedData) { result in
+                switch result {
+                case .success:
+                    completion(.success(()))
+                case .failure(let failure):
+                    completion(.failure(.error))
+                }
+            }
         }
-        
-        return keychainService.storeData(encryptedData)
     }
     
-    func retrieveAndDecryptToken() -> String? {
-        guard let encryptedData = keychainService.retrieveData(),
-              let decryptedData = secureEnclaveService.decryptData(data: encryptedData),
-              let token = String(data: decryptedData, encoding: .utf8) else {
-            print("❌ 토큰 복호화 실패")
-            return nil
+    func retrieveAndDecryptToken(completion: @escaping (Result<String, SecureError>) -> Void) {
+        keychainService.retrieveData { result in
+            switch result {
+            case .success(let encryptedData):
+                DispatchQueue.global().async { [weak self] in
+                    guard let self else { return }
+                    if let decryptedData = secureEnclaveService.decryptData(data: encryptedData),
+                       let token = String(data: decryptedData, encoding: .utf8) {
+                        print("✅ 복호화된 토큰: \(token)")
+                        completion(.success(token))
+                    } else {
+                        print("❌ 토큰 복호화 실패")
+                        completion(.failure(.error))
+                    }
+                }
+            case .failure(let failure):
+                print("❌ 토큰 복호화 실패")
+                completion(.failure(.error))
+            }
         }
-        
-        print("✅ 복호화된 토큰: \(token)")
-        return token
     }
     
-    func deleteToken() {
-        let isDelete = keychainService.deleteData()
-        if isDelete {
-            print("✅ Keychain 데이터 삭제 완료")
-        } else {
-            print("❌ Keychain 데이터 삭제 실패")
+    func deleteToken(completion: @escaping (Result<Void, SecureError>) -> Void) {
+        keychainService.deleteData { result in
+            switch result {
+            case .success:
+                print("✅ Keychain 데이터 삭제 완료")
+                completion(.success(()))
+            case .failure:
+                print("❌ Keychain 데이터 삭제 실패")
+                completion(.failure(.error))
+            }
+        }
+    }
+}
+
+final class TestSecureTokenManager: SecureTokenManager {
+    private let secureEnclaveService: MockSecureEnclaveService
+    private let keychainService: KeychainService
+    
+    init(
+        secureEnclaveService: MockSecureEnclaveService,
+        keychainService: KeychainService
+    ) {
+        self.secureEnclaveService = secureEnclaveService
+        self.keychainService = keychainService
+    }
+    
+    func encryptAndStoreToken(token: String, completion: @escaping (Result<Void, SecureError>) -> Void) {
+        DispatchQueue.global().async { [weak self] in
+            guard let self else { return }
+            
+            guard let tokenData = token.data(using: .utf8),
+                  let encryptedData = secureEnclaveService.encryptData(data: tokenData) else {
+                print("❌ 토큰 암호화 실패")
+                completion(.failure(.error))
+                return
+            }
+            
+            keychainService.storeData(encryptedData) { result in
+                switch result {
+                case .success:
+                    completion(.success(()))
+                case .failure(let failure):
+                    completion(.failure(.error))
+                }
+            }
+        }
+    }
+    
+    func retrieveAndDecryptToken(completion: @escaping (Result<String, SecureError>) -> Void) {
+        keychainService.retrieveData { result in
+            switch result {
+            case .success(let encryptedData):
+                DispatchQueue.global().async { [weak self] in
+                    guard let self else { return }
+                    if let decryptedData = secureEnclaveService.decryptData(data: encryptedData),
+                       let token = String(data: decryptedData, encoding: .utf8) {
+                        print("✅ 복호화된 토큰: \(token)")
+                        completion(.success(token))
+                    } else {
+                        print("❌ 토큰 복호화 실패")
+                        completion(.failure(.error))
+                    }
+                }
+            case .failure(let failure):
+                print("❌ 토큰 복호화 실패")
+                completion(.failure(.error))
+            }
+        }
+    }
+    
+    func deleteToken(completion: @escaping (Result<Void, SecureError>) -> Void) {
+        keychainService.deleteData { result in
+            switch result {
+            case .success:
+                print("✅ Keychain 데이터 삭제 완료")
+                completion(.success(()))
+            case .failure:
+                print("❌ Keychain 데이터 삭제 실패")
+                completion(.failure(.error))
+            }
         }
     }
 }
