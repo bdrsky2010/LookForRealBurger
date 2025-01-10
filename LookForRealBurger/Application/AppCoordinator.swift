3b50408bc8e9f0b0b1dca0e7cdbb704981353e1c
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2025-01-10 22:57:32 +0900

    add: 앱 최상위 Coordinator 정의

--- LookForRealBurger/Application/AppCoordinator.swift
+++ LookForRealBurger/Application/AppCoordinator.swift
@@ -1,4 +1,40 @@
 //
+//  AppCoordinator.swift
+//  LookForRealBurger
+//
+//  Created by Minjae Kim on 1/10/25.
+//
+
+import UIKit
+
+final class AppCoordinator: Coordinator {
+    var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    
+    private var navigationController: UINavigationController!
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        startAuthCoordinator()
+    }
+}
+
+extension AppCoordinator {
+    func startAuthCoordinator() {
+        let authCoordinator = AuthCoordinator(navigationController: navigationController)
+
+        childCoordinators.removeAll()
+        childCoordinators.append(authCoordinator)
+        
+        authCoordinator.parentCoordinator = self
+        authCoordinator.start()
+    }
+    
+}
+
 final class AuthCoordinator: Coordinator {
     var parentCoordinator: Coordinator?
     var childCoordinators: [Coordinator] = []
